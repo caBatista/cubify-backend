9c73676879a0525d5529c27ceb8bd26dee20c4f8
caio.batista <caio.araujo@edu.pucrs.br>
2024-10-10 16:17:41 -0300

    Initial Commit

---
+++ src/main/java/com/cubify/CubifyApplication.java
@@ -0,0 +1,13 @@
+package com.cubify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubifyApplication.class, args);
+	}
+
+}
